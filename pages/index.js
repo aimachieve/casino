5a88cf2b4227ffe1d6958d5ab4279c6645ebc56d
aimachieve <aim1005achieve@gmail.com>
2022-11-09 03:07:45 +0500

    ..

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import SecondNav from '../components/secondNav'
 import styles from '../styles/Home.module.css'
+import Dashboard from './dashboard'
 
 export default function Home() {
   return (
@@ -10,8 +12,8 @@ export default function Home() {
         <meta name="description" content="casino" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-     
+      <SecondNav />
+      <Dashboard />
     </div>
   )
 }
